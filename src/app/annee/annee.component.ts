d723f635dc4b06722a1a3e50a62da43baee33cc3
root <root@iago>
2017-04-19 22:30:15 +0200

    ajout de l annee pour la création de l aganda

---
+++ src/app/annee/annee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pr-annee',
+  templateUrl: './annee.component.html',
+  styleUrls: ['./annee.component.css']
+})
+export class AnneeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
